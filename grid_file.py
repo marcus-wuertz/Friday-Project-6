b2ad036776e3227a430794e4ef0d522d350ba7b0
Marcus Wuertz <mar2002cus@gmail.com>
2024-03-24 14:26:03 -0500

    Added label and entry box for username and corrected font family so that font size is displayed correctly

--- grid_file.py
+++ grid_file.py
@@ -6,7 +6,7 @@ root = Tk()
 root.title("Sign up page")
 
 # create widget for welcome message
-label1= ttk.Label(root, text='New? Sign up here!', font=(30))
+label1= ttk.Label(root, text='New? Sign up here!', font=('TkDefaultFont',20))
 label1.grid(row=0,column=2)
 
 #create a widget for returning users
@@ -14,9 +14,16 @@ returnBtn=ttk.Button(root, text='Returning user? Click here to log in')
 returnBtn.grid(row=2, column=2)
 
 # create labels and entry boxes for Name
-nameLbl= ttk.Label(root, text='Name', font=(18))
+nameLbl= ttk.Label(root, text='Name', font=('TkDefaultFont',16))
 nameLbl.grid(row=3, column=1)
 
 nameEntry=ttk.Entry(root)
 nameEntry.grid(row=3,column=2)
+
+# create label and entry box for username
+userNameLbl= ttk.Label(root, text='Username', font=('TkDefaultFont',16))
+userNameLbl.grid(row=4, column=1)
+
+userNameEntry=ttk.Entry(root)
+userNameEntry.grid(row=4,column=2)
 root.mainloop()

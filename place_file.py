73368e32bd76e9d74c14714e9203cd5d8e72521f
Marcus Wuertz <mar2002cus@gmail.com>
2024-03-24 15:25:18 -0500

    Added welcome message prompting users to log in

--- place_file.py
+++ place_file.py
@@ -10,3 +10,6 @@ window_width = 400
 window_height = 300
 root.geometry(f"{window_width}x{window_height}")
 
+# create widget for welcome message
+label1= ttk.Label(root, text='Returning user? Log in here!', font=('TkDefaultFont',20))
+label1.place(x=25, y=25)

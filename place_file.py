031b7da9caef51a5f16fae3a0d48e9e58fd90033
Marcus Wuertz <mar2002cus@gmail.com>
2024-03-24 15:24:16 -0500

    set the size of the GUI window so that users don't have to open full page to see the log in information

--- place_file.py
+++ place_file.py
@@ -4,3 +4,9 @@ from tkinter import ttk
 # Establish parent widget
 root = Tk()
 root.title("Log in page")
+
+# Set the size of the GUI window
+window_width = 400
+window_height = 300
+root.geometry(f"{window_width}x{window_height}")
+

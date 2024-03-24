cbb7b72a1e76c44fca5aaab81c01738e8a312f30
Marcus Wuertz <mar2002cus@gmail.com>
2024-03-24 13:28:42 -0500

    Added buttons for numbers to the left side of the GUI

--- pack_file.py
+++ pack_file.py
@@ -27,3 +27,29 @@ calcBtnEqual=ttk.Button(root, text='=')
 calcBtnEqual.pack(side='right')
 
 # create widget for numbers in the calculator
+calcBtn1=ttk.Button(root, text='1')
+calcBtn1.pack(side='left')
+
+calcBtn2=ttk.Button(root, text='2')
+calcBtn2.pack(side='left')
+
+calcBtn3=ttk.Button(root, text='3')
+calcBtn3.pack(side='left')
+
+calcBtn4=ttk.Button(root, text='4')
+calcBtn4.pack(side='left')
+
+calcBtn5=ttk.Button(root, text='5')
+calcBtn5.pack(side='left')
+
+calcBtn6=ttk.Button(root, text='6')
+calcBtn6.pack(side='left')
+
+calcBtn7=ttk.Button(root, text='7')
+calcBtn7.pack(side='left')
+
+calcBtn8=ttk.Button(root, text='8')
+calcBtn8.pack(side='left')
+
+calcBtn9=ttk.Button(root, text='9')
+calcBtn9.pack(side='left')

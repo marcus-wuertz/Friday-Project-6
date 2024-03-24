7d257f567a25741b2426fa3fcad973e1fcb8b10f
Marcus Wuertz <mar2002cus@gmail.com>
2024-03-24 13:15:22 -0500

    Added division button to the right side of the GUI

--- pack_file.py
+++ pack_file.py
@@ -8,3 +8,7 @@ root = Tk()
 #create widget to make entry box for the calculator
 calcOutput=ttk.Entry(root)
 calcOutput.pack(side='top')
+
+# create widget to add buttons for +,-,*,and /
+calcBtnDivide=ttk.Button(root, text='÷')
+calcBtnDivide.pack(side='right')

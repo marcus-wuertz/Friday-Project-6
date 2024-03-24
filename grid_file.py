528b98784532d5e2909bc540587866fda12700f5
Marcus Wuertz <mar2002cus@gmail.com>
2024-03-24 14:09:47 -0500

    added label to welcome new users to sign in and a button for returning users to log in

--- grid_file.py
+++ grid_file.py
@@ -4,3 +4,11 @@ from tkinter import ttk
 # Establish parent widget
 root = Tk()
 root.title("Sign up page")
+
+# create widget for welcome message
+label1= ttk.Label(root, text='New? Sign up here!', font=(30))
+label1.grid(row=0,column=2)
+
+#create a widget for returning users
+returnBtn=ttk.Button(root, text='Returning user? Click here to log in')
+returnBtn.grid(row=2, column=2)
